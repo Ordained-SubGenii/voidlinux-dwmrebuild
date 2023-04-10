6d8ca48ec622a8370726ce7620b0f2da3d7cdb24
Ordained-SubGenii <jedisocialist@protonmail.com>
2023-04-10 18:07:48 +0200

    removed double swaptag defs

--- dwm.c
+++ dwm.c
@@ -3202,28 +3202,6 @@ zoom(const Arg *arg)
 	pop(c);
 }
 
-void
-swaptags(const Arg *arg)
-{
-	unsigned int newtag = arg->ui & TAGMASK;
-	unsigned int curtag = selmon->tagset[selmon->seltags];
-
-	if (newtag == curtag || !curtag || (curtag & (curtag-1)))
-		return;
-
-	for (Client *c = selmon->clients; c != NULL; c = c->next) {
-		if((c->tags & newtag) || (c->tags & curtag))
-			c->tags ^= curtag ^ newtag;
-
-		if(!c->tags) c->tags = newtag;
-	}
-
-	selmon->tagset[selmon->seltags] = newtag;
-
-	focus(NULL);
-	arrange(selmon);
-}
-
 int
 main(int argc, char *argv[])
 {
