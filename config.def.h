d3264ed8e52228775491cfaaa003af5e37f31bbe
Ordained-SubGenii <jedisocialist@protonmail.com>
2023-04-10 16:00:31 +0200

    applied tatami patch directly by adding tatami.c inline with dwm.c and removing include tatami.c dir in config.def.h

--- config.def.h
+++ config.def.h
@@ -49,7 +49,7 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#include "tatami.c"
+/*  #include "tatami.c"  */
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
